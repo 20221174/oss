1a851bf0406860e6288271c013ae2e0425357d9f
seo-yoon <20221174@sungshin.ac.kr>
2024-11-15 02:13:35 +0900

    feat: jenkins docker pipline test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello docker and jenkins!!\n');
+  res.send('jenkins docker pipline test!!\n');
 });
 
 app.listen(PORT);
